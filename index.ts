455c120b35d13f1be772eba7b2ca45b9f9d3289b
charles montgomery <cmfurious@dotfury.com>
2021-01-14 20:34:08 +0900

    feat(dependencies): add mongo db

--- index.ts
+++ index.ts
@@ -3,8 +3,14 @@ import Express from 'express';
 const app = Express();
 const PORT = 8000;
 
+app.use(Express.json());
+
 app.get('/', (req, res) => {
-  res.send('Typescript + Express!!!!');
+  res.json({message: 'Typescript + Express!!!!'});
+});
+
+app.post('/', (req, res) => {
+  res.send(req.body);
 });
 
 app.listen(PORT, () => {
