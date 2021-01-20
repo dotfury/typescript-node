ad0fdb8e4ccd9b08873402e22ba78900e10edbc7
charles montgomery <cmfurious@dotfury.com>
2021-01-20 19:22:34 +0900

    add config for env vars, etc.

---
+++ src/config/index.ts
@@ -0,0 +1,26 @@
+import dotenv from 'dotenv-flow';
+
+dotenv.config();
+
+const PORT = process.env.PORT;
+const DB_USER = process.env.MONGODB_USER_NAME;
+const DB_PASSWORD = process.env.MONGODB_PASSWORD;
+const DB_NAME = process.env.MONGODB_DB_NAME;
+
+const MONGO_DB = {
+  user: DB_USER,
+  password: DB_PASSWORD,
+  database: DB_NAME,
+  url: `mongodb+srv://${DB_USER}:${DB_PASSWORD}@generic.6gly7.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`
+};
+
+const SERVER = {
+  port: PORT
+};
+
+const config = {
+  MONGO_DB,
+  SERVER
+};
+
+export default config;
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,14 @@
 import Express from 'express';
 import mongoose from 'mongoose';
-import dotenv from 'dotenv-flow';
+import config from './config';
 
 // routes
 import quotesRouter from './routes/quotes';
 
-dotenv.config();
-
+const PORT = config.SERVER.port;
 const app = Express();
-const PORT = process.env.PORT;
-const DB_USER = process.env.MONGODB_USER_NAME;
-const DB_PASSWORD = process.env.MONGODB_PASSWORD;
-const DB_NAME = process.env.MONGODB_DB_NAME;
 
-const DB_CONNECT_STRING = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@generic.6gly7.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
-mongoose.connect(DB_CONNECT_STRING, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(config.MONGO_DB.url, { useNewUrlParser: true, useUnifiedTopology: true });
 const client = mongoose.connection;
 
 client.once('open', () => {
